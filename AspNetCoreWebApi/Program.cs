680edba816d641abe7b69e6a8af9b6b479c822c3
takumura <takumura@outlook.com>
2021-12-29 14:25:25 +0900

    feat: register background service

--- AspNetCoreWebApi/Program.cs
+++ AspNetCoreWebApi/Program.cs
@@ -1,3 +1,5 @@
+using Net6MarkdownWebEngine.Backend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// // Add md2json converter service
+//builder.Services.AddSingleton<IService, MarkdownConverterService>();
+
+// Add Documents Watch Service
+var contetRootPath = builder.Environment.ContentRootPath;
+var isDevelopment = builder.Environment.IsDevelopment();
+builder.Services.Configure<DocumentsWatchServiceOptions> (options =>
+{
+    // TODO: review correct input/output directory path
+    options.InputDirectry = Path.Combine(contetRootPath, "../AngularStandalone/src/assets/docs");
+    if (isDevelopment)
+    {
+        options.OutputDirectry = Path.Combine(contetRootPath, "../AngularStandalone/src/assets/json");
+    }
+    else
+    {
+        options.OutputDirectry = Path.Combine(contetRootPath, "../AngularStandalone/dist/assets/json");
+    }
+});
+builder.Services.AddSingleton<Microsoft.Extensions.Hosting.IHostedService, DocumentsWatchService>();
 
 var app = builder.Build();
 
---
+++ AspNetCoreWebApi/Services/DocumentsWatchService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
+
+namespace Net6MarkdownWebEngine.Backend.Services;
+
+public class DocumentsWatchService : BackgroundService
+{
+    private readonly ILogger<DocumentsWatchService> _logger;
+    readonly IOptions<DocumentsWatchServiceOptions> _options;
+
+    public DocumentsWatchService(ILogger<DocumentsWatchService> logger,
+        IOptions<DocumentsWatchServiceOptions> options
+        //MarkdownConverterService service,
+        )
+    {
+        this._logger = logger;
+        this._options = options;
+        //this._service = service;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken token)
+    {
+        var now = DateTime.Now;
+
+        while (!token.IsCancellationRequested)
+        {
+            _logger.LogInformation($"Start md2json conversion for updated file.");
+            try
+            {
+                //await _service.ConvertAsync(_options.Value.InputDirectry, _options.Value.OutputDirectry, now).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "");
+            }
+
+            _logger.LogInformation("Watch documents updated after {now}", DateTime.Now);
+            var tcs = GetFileProviderTask();
+            using (token.Register(() =>
+            {
+                // this callback will be executed when token is cancelled
+                _logger.LogInformation($"token is cancelled!");
+                tcs.TrySetCanceled();
+            }))
+            {
+                // wait until any document change event happens fro file provider
+                await tcs.Task.ConfigureAwait(false);
+            }
+        }
+
+    }
+
+    private TaskCompletionSource GetFileProviderTask()
+    {
+        var fileProvider = new PhysicalFileProvider(_options.Value.InputDirectry);
+        var tcs = new TaskCompletionSource();
+        var token = fileProvider.Watch("**");
+
+        token.RegisterChangeCallback(state =>
+            ((TaskCompletionSource)state).TrySetResult(), tcs);
+
+        return tcs;
+    }
+}
