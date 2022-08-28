8ec73593c900ef2c618aafee5662b6c2b71fd1b4
takumura <takumura@outlook.com>
2022-08-28 22:00:44 +0900

    feat: show gist using postscribe

--- AngularStandalone/src/app/shared/markdown/document/document.component.ts
+++ AngularStandalone/src/app/shared/markdown/document/document.component.ts
@@ -1,10 +1,19 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Store } from '@ngrx/store';
 import { Observable, Subject, takeUntil } from 'rxjs';
 
 import { selectDocument } from 'src/app/markdown-document/store/markdown-document.selectors';
 import { DocumentRef } from 'src/app/store/models/document-ref.model';
+import postscribe from 'postscribe';
 
 @Component({
   selector: 'app-document',
@@ -13,22 +22,39 @@ import { DocumentRef } from 'src/app/store/models/document-ref.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DocumentComponent implements OnInit, OnDestroy {
+  @ViewChild('mdContent')
+  mdContentRef: ElementRef | undefined;
+
   documentTitle: string = '';
   safeMdContent: SafeHtml | undefined;
   private document$: Observable<DocumentRef> = this.store.select(selectDocument);
   private onDestroy = new Subject<void>();
 
-  constructor(private store: Store, private sanitizer: DomSanitizer) {}
+  constructor(private store: Store, private sanitizer: DomSanitizer, private cdRef: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.document$.pipe(takeUntil(this.onDestroy)).subscribe((x) => {
       this.documentTitle = x.content.title;
       this.safeMdContent = this.sanitizer.bypassSecurityTrustHtml(x.content.bodyHtml);
+
+      // detect change to update virtual DOM and allow to access mdContentRef
+      this.cdRef.detectChanges();
+
+      this.showGist();
     });
   }
 
   ngOnDestroy() {
     this.onDestroy.next();
     this.onDestroy.complete();
+  }
+
+  private showGist() {
+    if (this.mdContentRef) {
+      const gists = this.mdContentRef.nativeElement.querySelectorAll('div.gist');
+      gists.forEach((gist: HTMLDivElement) => {
+        postscribe(gist, gist.innerHTML);
+      });
+    }
   }
 }
