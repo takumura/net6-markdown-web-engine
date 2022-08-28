5f41799c18b13d4e7630dbc3bf79dfa8af052040
takumura <takumura@outlook.com>
2022-08-28 20:54:28 +0900

    refator: adjust document list item design for small screen

--- AngularStandalone/src/app/shared/shared.module.ts
+++ AngularStandalone/src/app/shared/shared.module.ts
@@ -6,13 +6,14 @@ import { MaterialModule } from './material.module';
 import { DocumentComponent } from './markdown/document/document.component';
 import { DocumentHeaderComponent } from './markdown/document-header/document-header.component';
 import { DocumentListComponent } from './lists/document-list/document-list.component';
-import { DocumentListItemComponent } from './lists/document-list/document-list-item/document-list-item.component';
+import { DocumentListItemComponent } from './lists/document-list-item/document-list-item.component';
 import { ExpansionDocumentListComponent } from './lists/expansion-document-list/expansion-document-list.component';
 import { LoadingBarComponent } from './loading-bar/loading-bar.component';
+import { LoadingBarService } from './loading-bar/loading-bar.service';
+import { BreakpointObserverService } from './services/breakpoint-observer.service';
+import { MyTitleStrategyService } from './services/markdown-document-title-strategy.service';
 import { TagComponent } from './tags/tag/tag.component';
 import { TagListComponent } from './tags/tag-list/tag-list.component';
-import { LoadingBarService } from './loading-bar/loading-bar.service';
-import { MyTitleStrategyService } from './services/markdown-document-title-strategy.service';
 
 @NgModule({
   declarations: [
@@ -36,6 +37,10 @@ import { MyTitleStrategyService } from './services/markdown-document-title-strat
     TagComponent,
     TagListComponent,
   ],
-  providers: [LoadingBarService, { provide: TitleStrategy, useClass: MyTitleStrategyService }],
+  providers: [
+    BreakpointObserverService,
+    LoadingBarService,
+    { provide: TitleStrategy, useClass: MyTitleStrategyService },
+  ],
 })
 export class SharedModule {}
